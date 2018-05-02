41f8c92287a5449dc7f7cee50a553d8a956b4a07
michael hohl <mhohl@rocketmail.com>
2018-05-02 18:41:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,4 +58,18 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+  end
+
+  def self.students_below_12th_grade
+  end
+
+  def self.first_X_students_in_grade_10(number)
+  end
+
+  def.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
+
 end
