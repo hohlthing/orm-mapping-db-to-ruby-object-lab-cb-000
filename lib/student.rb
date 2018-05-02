7a7bc9121279af063bc129ec9f1cfb9198241bf9
michael hohl <mhohl@rocketmail.com>
2018-05-02 18:34:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,9 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, self.name)
+    DB[:conn].execute(sql, self.name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
