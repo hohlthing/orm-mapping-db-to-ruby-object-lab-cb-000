a122f7cc2791fcf3789ea78707cd9c4e32c4d6e3
michael hohl <mhohl@rocketmail.com>
2018-05-02 18:58:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,7 +97,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    first_X_students_in_grade_10(10).first
+    first_X_students_in_grade_10(1)
   end
 
   def self.all_students_in_grade_X(grade)
